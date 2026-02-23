1d07b5e67f9c28d486af3e6879d49edc0391a405
seyxhel <sealtieljosephbpelagio@iskolarngbayan.pup.edu.ph>
2026-02-23 09:59:37 +0800

    feat: add seed_users management command to create default user accounts

---
+++ backend/users/management/commands/seed_users.py
@@ -0,0 +1,52 @@
+from django.core.management.base import BaseCommand
+from users.models import User
+
+
+SEED_USERS = [
+    {
+        'username': 'ivo',
+        'email': 'rivovebayo@gmail.com',
+        'password': 'RivoVebayo123!',
+        'phone': '+639666666666',
+        'role': User.ROLE_CLIENT,
+        'is_agreed_privacy_policy': True,
+    },
+    {
+        'username': 'seyxhel',
+        'email': 'sethpelagio20@gmail.com',
+        'password': 'Oklangyan421!',
+        'phone': '+639692221370',
+        'role': User.ROLE_ADMIN,
+        'is_staff': True,
+        'is_superuser': True,
+        'is_agreed_privacy_policy': True,
+    },
+    {
+        'username': 'gerardquadra',
+        'email': 'gerard.quadra@gmail.com',
+        'password': 'GerardQuadra123!',
+        'phone': '+639696969696',
+        'role': User.ROLE_EMPLOYEE,
+        'is_agreed_privacy_policy': True,
+    },
+]
+
+
+class Command(BaseCommand):
+    help = 'Seed the users_user table with default client, admin, and employee accounts.'
+
+    def handle(self, *args, **options):
+        for data in SEED_USERS:
+            password = data.pop('password')
+            user, created = User.objects.get_or_create(
+                username=data['username'],
+                defaults=data,
+            )
+            if created:
+                user.set_password(password)
+                user.save()
+                self.stdout.write(self.style.SUCCESS(f'Created {user.role} user: {user.username}'))
+            else:
+                self.stdout.write(self.style.WARNING(f'User already exists: {user.username}'))
+            # Re-add password key for idempotency
+            data['password'] = password
