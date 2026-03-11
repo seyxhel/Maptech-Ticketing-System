5843e8099e9f8201d1618a7b680e96908e001326
Gknightt <rivovebayo@gmail.com>
2026-03-11 13:15:30 +0800

    fix: Update environment variable handling to use BACKEND_TARGET in nginx configuration

--- Maptech_FrontendUI-main/entrypoint.sh
+++ Maptech_FrontendUI-main/entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 set -e
 
-# Provide defaults if not set
-: "${BACKEND_HOST:=backend}"
-: "${BACKEND_PORT:=8000}"
+# Build BACKEND_TARGET from BACKEND_URL (preferred) or from components
+if [ -n "${BACKEND_URL}" ]; then
+	# strip trailing slashes
+	BACKEND_URL_STRIPPED=$(printf '%s' "${BACKEND_URL}" | sed 's:/\+$::')
+	BACKEND_TARGET=${BACKEND_URL_STRIPPED}
+else
+	BACKEND_SCHEME=${BACKEND_SCHEME:-http}
+	BACKEND_HOST=${BACKEND_HOST:-backend}
+	BACKEND_PORT=${BACKEND_PORT:-8000}
+	BACKEND_TARGET=${BACKEND_SCHEME}://${BACKEND_HOST}:${BACKEND_PORT}
+fi
 
-# Substitute environment variables in nginx config template
-envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+export BACKEND_TARGET
+
+# Substitute BACKEND_TARGET into nginx config template
+envsubst '${BACKEND_TARGET}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
